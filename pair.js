828753101b9247e73a2e0ecffa3be3623b0c917a
asadaliasad0 <usamaijaz2030@gmail.com>
2024-08-21 08:54:59 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -58,7 +58,7 @@ router.get('/', async (req, res) => {
 
                let GIFTED_MD_TEXT = `
 ┏━━━━━━━━━━━━━━
-┃QUEEN_ANITA_V2 SESSION IS 
+┃HACKER-ALI SESSION IS 
 ┃SUCCESSFULLY
 ┃CONNECTED ✅🔥
 ┗━━━━━━━━━━━━━━━
